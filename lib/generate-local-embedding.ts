82544c9cb31f8d599ef8b0b4efb5369aa259eb6b
canyadigit29 <canyadigit29@gmail.com>
2025-05-28 09:57:20 -0400

    Update generate-local-embedding.ts

--- lib/generate-local-embedding.ts
+++ lib/generate-local-embedding.ts
@@ -1,14 +1,17 @@
-import OpenAI from "openai";
+import { pipeline } from "@xenova/transformers"
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-});
+export async function generateLocalEmbedding(content: string) {
+  const generateEmbedding = await pipeline(
+    "feature-extraction",
+    "Xenova/all-MiniLM-L6-v2"
+  )
 
-export async function generateLocalEmbedding(text: string) {
-  const response = await openai.embeddings.create({
-    model: "text-embedding-3-small",
-    input: text
-  });
+  const output = await generateEmbedding(content, {
+    pooling: "mean",
+    normalize: true
+  })
 
-  return response.data[0].embedding;
+  const embedding = Array.from(output.data)
+
+  return embedding
 }
