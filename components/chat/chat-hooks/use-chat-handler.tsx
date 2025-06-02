aebd1e7f9d2d96355546ec1423e5e11ddf612bb5
canyadigit29 <canyadigit29@gmail.com>
2025-06-02 10:27:38 -0400

    Update use-chat-handler.tsx

--- components/chat/chat-hooks/use-chat-handler.tsx
+++ components/chat/chat-hooks/use-chat-handler.tsx
@@ -290,6 +290,8 @@ export const useChatHandler = () => {
         assistant: selectedChat?.assistant_id ? selectedAssistant : null,
         messageFileItems: retrievedFileItems,
         chatFileItems: chatFileItems,
+        // If you want, add retrievalContext to payload for buildFinalMessages
+        retrievalContext: retrievalContext
       }
 
       let generatedText = ""
