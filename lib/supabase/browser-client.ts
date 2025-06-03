d67da5bec96ba8e3e34afbd54b30033acfe007b9
canyadigit29 <canyadigit29@gmail.com>
2025-06-03 13:53:28 -0400

    c

--- lib/supabase/browser-client.ts
+++ lib/supabase/browser-client.ts
@@ -1,7 +1,6 @@
-import { Database } from "@/supabase/types"
 import { createBrowserClient } from "@supabase/ssr"
 
-export const supabase = createBrowserClient<Database>(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
