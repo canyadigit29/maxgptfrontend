481d4a0cf8033b26847206e320025a482eb472c6
canyadigit29 <canyadigit29@gmail.com>
2025-06-07 17:13:00 -0400

    Remove unused 'multiple' prop from Input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -6,11 +6,10 @@ export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, multiple, ...props }, ref) => {
+  ({ className, type, ...props }, ref) => {
     return (
       <input
         type={type}
-        multiple={multiple}
         className={cn(
           "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus:none flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
